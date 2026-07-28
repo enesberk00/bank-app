84fba8dd965f35b716826bd527e65c9599938932
enesberk00 <ebk904270@gmail.com>
2026-07-28 14:48:06 +0300

    genel tasarım oluşturuldu ve database bağlandı.

---
+++ BankApp-Api.Repository/Entities/CardType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BankApp_Api.Repository.Entities;
+
+public partial class CardType
+{
+    public short Id { get; set; }
+
+    public string CardTypesName { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    public DateTime? DeletedAt { get; set; }
+
+    public virtual ICollection<Card> Cards { get; set; } = new List<Card>();
+}
---
+++ BankApp-Api.Repository/Entities/TransactionType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BankApp_Api.Repository.Entities;
+
+public partial class TransactionType
+{
+    public short Id { get; set; }
+
+    public string TransactionTypeName { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    public DateTime? DeletedAt { get; set; }
+
+    public virtual ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
+}
